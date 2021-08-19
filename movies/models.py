f2e13db1e9ffa74b2494bd4e90833cccbf106f05
신우주 <newspace@sin-ujuui-MacBookPro.local>
2021-08-19 21:34:53 +0900

    movie2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models.fields import IntegerField
+from django.db.models.fields.related import ManyToManyField
 
-# Create your models here.
+class Actor(models.Model):
+        first_name = models.CharField(max_length=45)
+        last_name = models.CharField(max_length=45)
+        date_of_birth = models.DateField()
+
+        class Meta:
+            db_table = 'actors'
+        def __str__(self):
+            return self.first_name + self.last_name + str(self.date_of_birth)
+
+class Movie(models.Model):
+        title = models.CharField(max_length=45)
+        release_date = models.DateField()
+        running_time = models.IntegerField()
+        actor = models.ManyToManyField(Actor,related_name="movie")
+
+        class Meta:
+            db_table = 'movies'
+        def __str__(self):
+            return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from movies.models import Actor, Movie
+
+class ActorsView(View):
+    def post(self, request):
+        data        = json.loads(request.boby)
+        
+        actor       = Actor.objects.create(
+            first_name      = data['first_name'],
+            last_name       = data['last_name'],
+            date_of_birth   = data['date_of_birth'],
+        )
+        return JsonResponse({'MESSAGE':'CREATED'}, status=201)
+
+    def get(self, request):
+        actors   = Actor.objects.all()
+        results  = []
+        for actor in actors:
+            movies = actor.movie.all()
+            for movie in movies:
+                results.append(
+                    {
+                    "first_name"    : actor.first_name,
+                    "last_name"     : actor.last_name,
+                    "date_of_birth" : actor.date_of_birth,
+                    }
+                )
+        return JsonResponse({'results':results}, status=200)
+
+
+class MoviesView(View):
+    def post(self, request):
+        data        = json.loads(request,body)
+
+        movie       = Movie.objects.create(
+            
+            title           = data['title'],
+            release_data    = data['release_data'],
+            running_time    = data['running_time'],
+            )
+        return JsonResponse({'MESSAGE':'CREATED'}, status=201)
+
+    def get(self, request):
+        actors  = Movie.objects.all()
+        results  = []
+        for movie in actors:
+            results.append(
+                {
+                    "title"         : movie.title,
+                    "release_data"  : movie.release_date,
+                    "running_time"  : movie.running_time,
+                }
+            )
+        return JsonResponse({'results':results}, status=200)
